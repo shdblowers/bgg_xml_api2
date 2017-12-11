6ab11ac5301b842bf56db9a85fcaef8410deada6
Steven Blowers <shdblowers@gmail.com>
2017-12-11 22:25:11 +0000

    get full details from search and can look at things based on ID

--- lib/bgg_xml_api2.ex
+++ lib/bgg_xml_api2.ex
@@ -5,9 +5,16 @@ defmodule BggXmlApi2 do
   alias BggXmlApi2.Api, as: BggApi
 
   def search(query, opts \\ []) do
-    "/search?query=#{query}" 
+    "/search?query=#{query}"
     |> BggApi.get!()
     |> Map.get(:body)
-    |> xpath(~x"//item"l, name: ~x"./name[@type='primary']/@value", id: ~x"./@id")
+    |> xpath(~x"//item"l, id: ~x"./@id", name: ~x"./name[@type='primary']/@value", type: ~x"./@type", year_published: ~x"./yearpublished/@value")
+  end
+
+  def thing(id) do
+    "/thing?id=#{id}"
+    |> BggApi.get!()
+    |> Map.get(:body)
+    |> xpath(~x"//item", id: ~x"./@id", name: ~x"./name[@type='primary']/@value", min_players: ~x"./minplayers/@value", max_players: ~x"./maxplayers/@value")
   end
 end
