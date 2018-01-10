5c83a6dc56892b07f34556dc59d32d13f8aa03bc
Steven Blowers <sblowers@findmypast.com>
2018-01-10 15:05:35 +0000

    adding family resource with basic info

---
+++ lib/bgg_xml_api2/family.ex
@@ -0,0 +1,62 @@
+defmodule BggXmlApi2.Family do
+  @moduledoc """
+  Relating to families of things on BGG.
+  """
+
+  import SweetXml
+
+  alias BggXmlApi2.Api, as: BggApi
+
+  @enforce_keys [:id, :name, :type]
+
+  defstruct [
+    :id,
+    :name,
+    :type,
+    :description,
+    :image,
+    :thumbnail
+  ]
+
+  def info(id) do
+    family =
+      "/family?id=#{id}"
+      |> BggApi.get!()
+      |> Map.get(:body)
+      |> retrieve_family_details()
+
+    {:ok, struct(__MODULE__, family)}
+  end
+
+  defp retrieve_family_details(xml) do
+    xml
+    |> xpath(~x"//item")
+    |> rfd()
+  end
+
+  defp rfd(family) do
+    %{
+      id:
+        family
+        |> xpath(~x"./@id")
+        |> if_charlist_convert_to_string(),
+      name:
+        family
+        |> xpath(~x"./name[@type='primary']/@value")
+        |> if_charlist_convert_to_string(),
+      type:
+        family
+        |> xpath(~x"./@type")
+        |> if_charlist_convert_to_string()
+    }
+  end
+
+  defp if_charlist_convert_to_string(possible_charlist) do
+    if is_list(possible_charlist) do
+      List.to_string(possible_charlist)
+    else
+      possible_charlist
+    end
+  end
+
+end
---
+++ test/bgg_xml_api2/family_test.exs
@@ -0,0 +1,21 @@
+defmodule BggXmlApi2.FamilyTest do
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  alias BggXmlApi2.Family
+
+  setup_all do
+    HTTPoison.start()
+  end
+
+  test "family info" do
+    use_cassette "camels_info" do
+      assert Family.info("6480") ==
+               {:ok, %Family{
+                 id: "6480",
+                 name: "Animals: Camels",
+                 type: "boardgamefamily"
+               }}
+    end
+  end
+end
